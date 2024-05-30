2e7bbea47362f12ddebad1717582b9d9ae46ca99
PashchenkoIvan <vanapasenko886@gmail.com>
2024-05-31 01:19:59 +0300

    create getTrendingMovie request

---
+++ tmdb/tmdb/Modules/Classes/StaticMethodsClass/StaticMethodsClass.swift
@@ -0,0 +1,24 @@
+//
+//  StaticMethodsClass.swift
+//  tmdb
+//
+//  Created by Пащенко Иван on 31.05.2024.
+//
+
+import Foundation
+
+class StaticMethodsClass {
+    public static func getUserData () -> ReturnUserDataStruct? {
+        
+        var userData: ReturnUserDataStruct?
+        
+        if let savedData = UserDefaults.standard.object(forKey: "userData") as? Data {
+            let decoder = JSONDecoder()
+            if let loadedData = try? decoder.decode(ReturnUserDataStruct.self, from: savedData) {
+                userData = loadedData
+            }
+        }
+        
+        return userData
+    }
+}
--- tmdb/tmdb/Modules/Views/Genres/GenresViewController.swift
+++ tmdb/tmdb/Modules/Views/Genres/GenresViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import Kingfisher
 
 class GenresViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        guard let userData: ReturnUserDataStruct = StaticMethodsClass.getUserData() else {
+            print("Error with getting user data")
+            return
+        }
+        
+        RequestClass.request(address: .GetTrendMovies, params: .GetTrendMovies(.init(requestType: .get, language: "en-US"))) { (responce: Result<TrendingMovieStruct, Error> ) in
+            switch responce {
+                
+            case .success(let result):
+                print(result)
+                
+            case .failure(let error):
+                print("Error with getting trending movies")
+                
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         navigationController?.navigationBar.topItem?.title = "Genres"
     }
 
